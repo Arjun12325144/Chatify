260c46f1ff0d953bca9ac0f2983d545d3c5e3928
Arjun12325144 <rajputarjun2901@gmail.com>
2025-11-03 18:38:45 +0530

    message routes added

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,7 +1,15 @@
 import express from 'express';
+import {getAllContacts, getMessagesByUserId, sendMessage,getChatPartners} from '../controllers/message.controller.js'; 
+import { protectRoute } from '../middleware/auth.middleware.js'
+import { arcjetProtection } from '../middleware/arcjet.middleware.js'
 const router = express.Router();
-router.get("/send",(req,res)=>{
-    res.send("send message endpoinit")
-})
+
+
+router.use(arcjetProtection, protectRoute)
+
+router.get("/contacts", getAllContacts);
+router.get("/chats", getChatPartners); // provide name that chats are going ebtwen
+router.get("/:id",getMessagesByUserId); // basically provide chats between two person so we want to provide id
+router.post("/send/:id", sendMessage) //the user we want to send the message
 
 export default router;
